5109c388657b018b85572a3a56d812e909a26178
davidcontey <77142902+davidcontey@users.noreply.github.com>
2024-08-21 12:24:17 -0400

    Add files via upload
    Added Garden of Plenty option, works for 7 days worth of trailblaze power.

---
+++ CalyxSimulationEXE/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace CalyxSimulationEXE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.purpleBox = new System.Windows.Forms.TextBox();
+            this.blueBox = new System.Windows.Forms.TextBox();
+            this.greenBox = new System.Windows.Forms.TextBox();
+            this.purpleLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.gardenOfPlentyBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(337, 415);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(75, 23);
+            this.confirmButton.TabIndex = 0;
+            this.confirmButton.Text = "Calculate";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // purpleBox
+            // 
+            this.purpleBox.Location = new System.Drawing.Point(112, 174);
+            this.purpleBox.Name = "purpleBox";
+            this.purpleBox.Size = new System.Drawing.Size(100, 20);
+            this.purpleBox.TabIndex = 1;
+            this.purpleBox.Text = "0";
+            // 
+            // blueBox
+            // 
+            this.blueBox.Location = new System.Drawing.Point(337, 174);
+            this.blueBox.Name = "blueBox";
+            this.blueBox.Size = new System.Drawing.Size(100, 20);
+            this.blueBox.TabIndex = 2;
+            this.blueBox.Text = "0";
+            // 
+            // greenBox
+            // 
+            this.greenBox.Location = new System.Drawing.Point(571, 174);
+            this.greenBox.Name = "greenBox";
+            this.greenBox.Size = new System.Drawing.Size(100, 20);
+            this.greenBox.TabIndex = 3;
+            this.greenBox.Text = "0";
+            // 
+            // purpleLabel
+            // 
+            this.purpleLabel.AutoSize = true;
+            this.purpleLabel.Location = new System.Drawing.Point(71, 180);
+            this.purpleLabel.Name = "purpleLabel";
+            this.purpleLabel.Size = new System.Drawing.Size(37, 13);
+            this.purpleLabel.TabIndex = 4;
+            this.purpleLabel.Text = "Purple";
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.AutoSize = true;
+            this.blueLabel.Location = new System.Drawing.Point(294, 180);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(28, 13);
+            this.blueLabel.TabIndex = 5;
+            this.blueLabel.Text = "Blue";
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.AutoSize = true;
+            this.greenLabel.Location = new System.Drawing.Point(529, 180);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(36, 13);
+            this.greenLabel.TabIndex = 6;
+            this.greenLabel.Text = "Green";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(297, 282);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(10, 13);
+            this.outputLabel.TabIndex = 7;
+            this.outputLabel.Text = " ";
+            // 
+            // gardenOfPlentyBox
+            // 
+            this.gardenOfPlentyBox.AutoSize = true;
+            this.gardenOfPlentyBox.Location = new System.Drawing.Point(337, 100);
+            this.gardenOfPlentyBox.Name = "gardenOfPlentyBox";
+            this.gardenOfPlentyBox.Size = new System.Drawing.Size(105, 17);
+            this.gardenOfPlentyBox.TabIndex = 8;
+            this.gardenOfPlentyBox.Text = "Garden of Plenty";
+            this.gardenOfPlentyBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gardenOfPlentyBox);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.purpleLabel);
+            this.Controls.Add(this.greenBox);
+            this.Controls.Add(this.blueBox);
+            this.Controls.Add(this.purpleBox);
+            this.Controls.Add(this.confirmButton);
+            this.Name = "Form1";
+            this.Text = "Calyx Simulation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.TextBox purpleBox;
+        private System.Windows.Forms.TextBox blueBox;
+        private System.Windows.Forms.TextBox greenBox;
+        private System.Windows.Forms.Label purpleLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.CheckBox gardenOfPlentyBox;
+    }
+}
+
---
+++ CalyxSimulationEXE/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalyxSimulationEXE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int greenGoal;
+        int blueGoal;
+        int purpleGoal;
+        Random random = new Random();
+
+        public int changeBluesFromGreens(ref int greens, int diff)
+        {
+            int newBlues = 0;
+            while (diff > 3)
+            {
+                diff -= 3;
+                greens -= 3;
+                newBlues++;
+            }
+            return newBlues;
+        }
+
+        public int changePurplesFromBlues(ref int blues, int diff)
+        {
+            int newPurples = 0;
+            while (diff > 3)
+            {
+                diff -= 3;
+                blues -= 3;
+                newPurples++;
+            }
+            return newPurples;
+        }
+
+        bool greenCheck(int green)
+        {
+            return green <= greenGoal;
+        }
+        bool blueCheck(int blue)
+        {
+            return blue <= blueGoal;
+        }
+        bool purpleCheck(int purple)
+        {
+            return purple <= purpleGoal;
+        }
+
+        double CalculateAverage(List<int> numbers)
+        {
+            if (numbers.Count == 0)
+            {
+                Console.Error.WriteLine("Error: The list is empty.");
+                outputLabel.Text = "Broke";
+                return 0.0;
+            }
+
+            int sum = numbers.Sum();
+            return (double)sum / numbers.Count;
+        }
+
+        void gardenOfPlenty(ref int greens, ref int blues, ref int purples)
+        {
+            // this is just the base without the consumption of trailblaze power
+            // trigger if trailblaze power is divisible by 240 ?
+            for(int i = 0; i < 12; i++)
+            {
+                int roll = 1 + (random.Next(100));
+                greens++;
+                if (roll < 21)
+                {
+                    greens++;
+                }
+                blues++;
+                roll = 1 + (random.Next(100));
+                if (roll < 15)
+                {
+                    purples++;
+                }
+            }
+            
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            bool allChecks = false;
+
+            greenGoal = int.Parse(greenBox.Text);
+            blueGoal = int.Parse(blueBox.Text);
+            purpleGoal = int.Parse(purpleBox.Text);
+
+            int day = 0;
+
+            int currentGreen = 0;
+            int currentBlue = 0;
+            int currentPurple = 0;
+
+            List<int> allPower =  new List<int>();
+            int trailblazePower = 0;
+
+         //   Random random = new Random();
+
+            
+
+            for(int i = 1; i < 100; i++)
+            {
+                allChecks = false;
+                currentGreen = 0;
+                currentBlue = 0; 
+                currentPurple = 0;
+                trailblazePower = 0;
+                while (!allChecks)
+                {
+                    int roll = 1+ (random.Next(100));
+                    currentGreen++;
+                    if (roll < 21)
+                    {
+                        currentGreen++;
+                    }
+                    currentBlue++;
+                    roll = 1 + (random.Next(100));
+                    if (roll < 15)
+                    {
+                        currentPurple++;
+                    }
+                    trailblazePower += 10;
+                    if (trailblazePower<=1680 && trailblazePower % 240 == 0 
+                        && gardenOfPlentyBox.Checked)
+                    {
+                        day++;
+                        gardenOfPlenty(ref currentGreen, ref currentBlue, ref currentPurple);
+                    }
+
+                    if (currentGreen > greenGoal)
+                    {
+                        int diff = currentGreen - greenGoal;
+                        if (diff > 3)
+                        {
+                            currentBlue += changeBluesFromGreens(ref currentGreen, diff);
+                        }
+                    }
+
+                    if (currentBlue > blueGoal)
+                    {
+                        int diff = currentBlue - blueGoal;
+                        if (diff > 3)
+                        {
+                            currentPurple += changePurplesFromBlues(ref currentBlue, diff);
+                        }
+                    }
+
+                    if(!greenCheck(currentGreen) && !blueCheck(currentBlue) && !purpleCheck(currentPurple))
+                    {
+                        allChecks = true;
+                    }
+                }
+                allPower.Add(trailblazePower);
+            }
+            double avg=Math.Round(CalculateAverage(allPower));
+
+            outputLabel.Text = "Average power used: " + avg.ToString() + 
+                ", takes " + Math.Round(avg/240,2) + " days, 100 simulations.";
+
+            //greenBox.Text = "0";
+            //blueBox.Text = "0";
+            //purpleBox.Text = "0";
+        }
+    }
+}
